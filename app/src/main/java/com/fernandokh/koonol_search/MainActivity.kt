a2a135bd43753adf4a59bd8a02a0af386e7a7d12
AnaTMorales <moralesgarciaanateresa@gmail.com>
2024-10-11 03:07:01 -0500

    logo

--- app/src/main/java/com/fernandokh/koonol_search/MainActivity.kt
+++ app/src/main/java/com/fernandokh/koonol_search/MainActivity.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.fernandokh.koonol_search.ui.theme.KoonolsearchTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +30,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             KoonolsearchTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface {
+
+                    SplashScreen {  }
                 }
             }
         }
@@ -31,17 +40,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun SplashScreen(onTimeout: () -> Unit) {
+    val isDarkTheme = isSystemInDarkTheme()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KoonolsearchTheme {
-        Greeting("Android")
+    LaunchedEffect(true) {
+        delay(3000)
+        onTimeout()
+    }
+
+    Surface(
+        color = if (isDarkTheme) MaterialTheme.colorScheme.background else MaterialTheme.colorScheme.surface,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(
+                    id = if (isDarkTheme) R.drawable.b_logodark else R.drawable.b_logolight
+                ),
+                contentDescription = "App Logo",
+                modifier = Modifier.size(150.dp)
+            )
+        }
     }
 }
